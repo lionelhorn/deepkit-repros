adec549da9dd26fa93aaa1daf22ea36fbb7595cf
Lionel <50528276+lionelhorn@users.noreply.github.com>
2023-10-19 23:26:21 +0000

    feat: add controller

--- src/app.ts
+++ src/app.ts
@@ -1,14 +1,14 @@
-import {ApiConsoleModule} from '@deepkit/api-console-module';
-import {App} from '@deepkit/app';
-import {eventDispatcher} from "@deepkit/event";
-import {FrameworkModule, onServerMainBootstrapDone} from '@deepkit/framework';
-import {LoggerInterface} from '@deepkit/logger';
-import {Database} from "@deepkit/orm";
-import {PostgresDatabaseAdapter} from "@deepkit/postgres";
-import {AutoIncrement, cast, entity, PrimaryKey} from "@deepkit/type";
-import "reflect-metadata";
+import { ApiConsoleModule } from "@deepkit/api-console-module";
+import { App } from "@deepkit/app";
+import { eventDispatcher } from "@deepkit/event";
+import { FrameworkModule, onServerMainBootstrapDone } from "@deepkit/framework";
+import { LoggerInterface } from "@deepkit/logger";
+import { Database } from "@deepkit/orm";
+import { PostgresDatabaseAdapter } from "@deepkit/postgres";
+import { AutoIncrement, cast, entity, PrimaryKey } from "@deepkit/type";
+import { TestController } from "./controller";
 
-@(entity.name('product')).collection("products")
+@entity.name("product").collection("products")
 export class Product {
   id: number & PrimaryKey & AutoIncrement = 0;
   raw?: { [key: string]: any };
@@ -18,18 +18,23 @@ export class Product {
 
 export class PostgresDatabase extends Database {
   constructor() {
-    super(new PostgresDatabaseAdapter({
-      host: '0.0.0.0',
-      port: 5432,
-      user: "postgres",
-      password: "root"
-    }), [Product]);
+    super(
+      new PostgresDatabaseAdapter({
+        host: "0.0.0.0",
+        port: 5432,
+        user: "postgres",
+        password: "root",
+      }),
+      [Product],
+    );
   }
 }
 
 class Bootstrap {
-  constructor(private database: PostgresDatabase, private logger: LoggerInterface) {
-  }
+  constructor(
+    private database: PostgresDatabase,
+    private logger: LoggerInterface,
+  ) {}
 
   // @ts-ignore
   @eventDispatcher.listen(onServerMainBootstrapDone)
@@ -39,21 +44,27 @@ class Bootstrap {
     // Seed some items
     const itemsInDb = await this.database.query(Product).filter().find();
     if (itemsInDb.length == 0) {
-      await this.database.persist(cast<Product>({raw: {productId: 1, name: "first"}}));
-      await this.database.persist(cast<Product>({raw: {productId: 2, name: "second"}}));
+      await this.database.persist(
+        cast<Product>({ raw: { productId: 1, name: "first" } }),
+      );
+      await this.database.persist(
+        cast<Product>({ raw: { productId: 2, name: "second" } }),
+      );
     }
 
     try {
-      console.log("Trying to get product with raw.productId")
-      const query = this.database.query(Product).filter({"raw.productId": 1});
+      console.log("Trying to get product with raw.productId");
+      const query = this.database.query(Product).filter({ "raw.productId": 1 });
       console.log(query.findOneOrUndefined());
     } catch (e: any) {
       console.log(e);
     }
 
     try {
-      console.log(`Trying to get product with {"raw": {productId: 1}`)
-      const query = this.database.query(Product).filter({"raw": {productId: 1}});
+      console.log(`Trying to get product with {"raw": {productId: 1}`);
+      const query = this.database
+        .query(Product)
+        .filter({ raw: { productId: 1 } });
       console.log(query.findOneOrUndefined());
     } catch (e: any) {
       console.log(e);
@@ -63,19 +74,16 @@ class Bootstrap {
 
 const app = new App({
   providers: [PostgresDatabase],
-  listeners: [
-    Bootstrap,
-  ],
-  controllers: [],
+  listeners: [Bootstrap],
+  controllers: [TestController],
   imports: [
     new FrameworkModule({
       debug: true,
     }),
     new ApiConsoleModule({
-      path: '/api'
+      path: "/api",
     }),
-  ]
+  ],
 });
 
-app.loadConfigFromEnv({prefix: 'APP_'})
-.run();
+app.loadConfigFromEnv({ prefix: "APP_" }).run();
